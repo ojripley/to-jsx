20ff13ba70824516e171c4202e72513264beb3f0
ojripley <ojripley19@gmail.com>
2020-07-25 20:18:01 -0400

    inital setup

---
+++ src/components/PasteBox.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function PasteBox() {
+
+  return (
+    <div onClick={console.log('do the thing')}>
+      Convert from clipboad!
+    </div>
+  );
+};
