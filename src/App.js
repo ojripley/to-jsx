7c62af2e7c3e199d802977447ec910cc5ca79bc8
ojripley <ojripley19@gmail.com>
2020-07-29 13:45:34 -0400

    header link to personal website

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <div className='header'>
         <p className='header-text'>to-JSX</p>
 
-        <p className='header-author'>Created By: <span><a className='author-link' href='https://github.com/ojripley' target='_blank' rel='noopener noreferrer'>Owen Ripley</a></span></p>
+        <p className='header-author'>Created By: <span><a className='author-link' href='https://ojripley.com/' target='_blank' rel='noopener noreferrer'>Owen Ripley</a></span></p>
       </div>
       <Converter></Converter>
     </div>
