42795bab4da6f050fd3d039a71a40f3a8f371e0b
ojripley <ojripley19@gmail.com>
2020-08-06 15:39:56 -0400

    viewport fit

--- src/components/ControlBar.js
+++ src/components/ControlBar.js
@@ -5,9 +5,17 @@ import './styles/controlBar.css';
 export default function ControlBar() {
 
   return(
-    <div className='control-bar'>
+    <p className='control-bar'>
+      <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-chevron-left" width="32" height="32" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#2c3e50" fill="none" strokeLinecap="round" strokeLinejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" />
+        <polyline points="15 6 9 12 15 18" />
+      </svg>
+
       <p className='control-item'>test</p>
-      
-    </div>
+      <p className='control-item'>test</p>
+      <p className='control-item'>test</p>
+      <p className='control-item'>test</p>
+      <p className='control-item'>test</p>
+    </p>
   );
 };
